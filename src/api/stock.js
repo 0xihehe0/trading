3005e94dc29853336b78451b93ca487cdf0dd5c5
yaojinxi <864554492@qq.com>
2026-04-22 17:19:57 +0800

    adjust api

--- src/api/stock.js
+++ src/api/stock.js
@@ -2,7 +2,7 @@
  * @Author: yaojinxi 864554492@qq.com
  * @Date: 2025-04-09 20:18:26
  * @LastEditors: yaojinxi 864554492@qq.com
- * @LastEditTime: 2025-09-03 22:56:28
+ * @LastEditTime: 2026-04-22 16:58:42
  * @FilePath: \trading\src\api\stock.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -21,12 +21,11 @@ export const getStockData = async (ticker, range, maArray = []) => {
   return res.data;
 };
 
-export const postNewdata = async (ticker,lastDay) => {
-  // 建议把前端上下文也传给后端，后端可直接返回同结构数据以便立即刷新图表
+export const postNewdata = async (ticker, lastDay) => {
   const payload = {
     ticker,
-    lastDay
+    lastDay,  // 如 "2025-04-17"，后端会自动从下一天开始拉
   };
-  const res = await axios.post('/newstock', payload); // 对应后端 /api/newstock
-  return res.data; // 期望后端也返回 { data, meta }；若只回 meta，App 里有兜底
+  const res = await axios.post('/newstock', payload);
+  return res.data; // { data, meta }
 };
